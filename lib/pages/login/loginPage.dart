d47944b565c4f3bf073d550d7b70425ffd784318
ItsAdityaKSingh <adityakksingh23@gmail.com>
2021-12-05 22:55:30 +0530

    commit on loginscreen

--- lib/pages/login/loginPage.dart
+++ lib/pages/login/loginPage.dart
@@ -69,172 +69,176 @@ class _LoginPageState extends State<LoginPage> {
         body: _loading
             ? Center(child: LoadingCircle)
             : ListView(
-          children: <Widget>[
-            SizedBox(height: 10,),
-            Column(
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(right: 10),
-                      child: CircleAvatar(
-                        radius: 30,
-                        backgroundImage: AssetImage('assets/COPS.png'),
-                      ),
-                    )
-                  ],
-                ),
-                SizedBox(height: 50,),
-                Container(
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      boxShadow: [
-                        BoxShadow(
-                          offset: Offset(1,1),
-                          color: Colors.black,
-                          spreadRadius: 2,
-                          blurRadius: 2,
-                        )
-                      ]
+                physics: const NeverScrollableScrollPhysics(),
+                children: <Widget>[
+                  SizedBox(
+                    height: 10,
                   ),
-                  child: CircleAvatar(
-                    radius: 140,
-                    backgroundImage: AssetImage('assets/LoginPageImage.jpg'),
-                  ),
-                ),
-                SizedBox(height: 20,),
-                Text(' Welcome to IIT(BHU) ',
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.halant(
-                      shadows: [
-                        Shadow(
-                          color: Colors.black,
-                          offset: Offset(1.0,1.0),
-                          blurRadius: 1,
-                        )
-                      ],
-                      color: Colors.black,
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.w600
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 30),
-            Padding(
-              padding: const EdgeInsets.only(left: 30,right: 30),
-              child: OutlinedButton(
-                style: outlineButtonStyle.copyWith(
-                  shape: MaterialStateProperty.all<OutlinedBorder>(
-                    RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(40)),
-                  ),
-                  side: MaterialStateProperty.resolveWith<BorderSide>(
-                          (states) {
-                        if (states.contains(MaterialState.pressed))
-                          return BorderSide(
-                            color: Theme.of(context).colorScheme.primary,
-                            width: 1,
-                          );
-                        return BorderSide(color: Colors.grey);
-                      }),
-                  elevation:
-                  MaterialStateProperty.resolveWith<double>((states) {
-                    if (states.contains(MaterialState.pressed)) return 0.0;
-                    return null;
-                  }),
-                  overlayColor: MaterialStateProperty.resolveWith(
-                        (states) {
-                      if (states.contains(MaterialState.pressed))
-                        return Colors.grey;
-                      return null;
-                    },
-                  ),
-                  foregroundColor: MaterialStateProperty.resolveWith<Color>(
-                        (states) {
-                      if (states.contains(MaterialState.pressed))
-                        return Colors.grey[400];
-                      return Colors.grey;
-                    },
-                  ),
-                ),
-                onPressed: AppConstants.logInButtonEnabled == false
-                    ? null
-                    : () {
-                       _signInWithGoogle();
-                       AppConstants.guestButtonEnabled=false;
-                    },
-
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 13, 0, 13),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
+                  Column(
                     children: <Widget>[
-                      Image(
-                          image: AssetImage("assets/google_logo.png"),
-                          height: 25.0),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10),
-                        child: Text(
-                          'Sign in with Google',
-                          style:
-                          TextStyle(fontSize: 20, color: Colors.black),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(right: 10),
+                            child: CircleAvatar(
+                              radius: 30,
+                              backgroundImage: AssetImage('assets/COPS.png'),
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Container(
+                        decoration:
+                            BoxDecoration(shape: BoxShape.circle, boxShadow: [
+                          BoxShadow(
+                            offset: Offset(1, 1),
+                            color: Colors.black,
+                            spreadRadius: 2,
+                            blurRadius: 2,
+                          )
+                        ]),
+                        child: CircleAvatar(
+                          radius: 140,
+                          backgroundImage:
+                              AssetImage('assets/LoginPageImage.jpg'),
                         ),
-                      )
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        ' Welcome to IIT(BHU) ',
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.halant(
+                            shadows: [
+                              Shadow(
+                                color: Colors.black,
+                                offset: Offset(1.0, 1.0),
+                                blurRadius: 1,
+                              )
+                            ],
+                            color: Colors.black,
+                            fontSize: 30.0,
+                            fontWeight: FontWeight.w600),
+                      ),
                     ],
                   ),
-                ),
+                  SizedBox(height: 30),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30, right: 30),
+                    child: OutlinedButton(
+                      style: outlineButtonStyle.copyWith(
+                        shape: MaterialStateProperty.all<OutlinedBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(40)),
+                        ),
+                        side: MaterialStateProperty.resolveWith<BorderSide>(
+                            (states) {
+                          if (states.contains(MaterialState.pressed))
+                            return BorderSide(
+                              color: Theme.of(context).colorScheme.primary,
+                              width: 1,
+                            );
+                          return BorderSide(color: Colors.grey);
+                        }),
+                        elevation:
+                            MaterialStateProperty.resolveWith<double>((states) {
+                          if (states.contains(MaterialState.pressed))
+                            return 0.0;
+                          return null;
+                        }),
+                        overlayColor: MaterialStateProperty.resolveWith(
+                          (states) {
+                            if (states.contains(MaterialState.pressed))
+                              return Colors.grey;
+                            return null;
+                          },
+                        ),
+                        foregroundColor:
+                            MaterialStateProperty.resolveWith<Color>(
+                          (states) {
+                            if (states.contains(MaterialState.pressed))
+                              return Colors.grey[400];
+                            return Colors.grey;
+                          },
+                        ),
+                      ),
+                      onPressed: AppConstants.logInButtonEnabled == false
+                          ? null
+                          : () {
+                              _signInWithGoogle();
+                              AppConstants.guestButtonEnabled = false;
+                            },
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 13, 0, 13),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Image(
+                                image: AssetImage("assets/google_logo.png"),
+                                height: 25.0),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10),
+                              child: Text(
+                                'Sign in with Google',
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.black),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'Login Using ',
+                        //style: TextStyle(fontFamily: 'Montserrat'),
+                        style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w400),
+                      ),
+                      Text(
+                        'Institute ID.',
+                        //style: TextStyle(fontFamily: 'Montserrat'),
+                        style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 120.0),
+                ],
               ),
-            ),
-            SizedBox(height: 10.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Login Using ',
-                  //style: TextStyle(fontFamily: 'Montserrat'),
-                  style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.black,
-                      fontWeight: FontWeight.w400),
-                ),
-                Text(
-                  'Institute ID.',
-                  //style: TextStyle(fontFamily: 'Montserrat'),
-                  style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            SizedBox(height: 120.0),
-          ],
-        ),
-
-        floatingActionButton:Visibility(
-          visible: AppConstants.guestButtonEnabled
-                  ? true
-                  : false ,
+        floatingActionButton: Visibility(
+          visible: AppConstants.guestButtonEnabled ? true : false,
           child: Container(
-          height: MediaQuery.of(context).size.width * 0.25,
-          width: MediaQuery.of(context).size.width * 0.25,
-          child: FloatingActionButton.extended(
-            onPressed: () async {
-              await LoginPage.guestLoginSetup();
-              Navigator.of(context).pushNamedAndRemoveUntil(
-                  '/home', ModalRoute.withName('/root'));
-            },
-            label: Text(
-              'Guest',
-              style: TextStyle(
-                fontSize: MediaQuery.of(context).size.width * 0.07,
-                fontWeight: FontWeight.bold,
+            height: MediaQuery.of(context).size.width * 0.20,
+            width: MediaQuery.of(context).size.width * 0.20,
+            child: FloatingActionButton.extended(
+              onPressed: () async {
+                await LoginPage.guestLoginSetup();
+                Navigator.of(context).pushNamedAndRemoveUntil(
+                    '/home', ModalRoute.withName('/root'));
+              },
+              label: Text(
+                'Guest',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width * 0.05,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            backgroundColor: Colors.black,
+              backgroundColor: Colors.black,
             ),
           ),
         ),
@@ -259,7 +263,7 @@ class _LoginPageState extends State<LoginPage> {
       if (_user == null || AppConstants.djangoToken == null) {
         setState(() {
           this._loading = false;
-          AppConstants.guestButtonEnabled=true;
+          AppConstants.guestButtonEnabled = true;
         });
 
         await authentication.signOutGoogle();
@@ -267,7 +271,7 @@ class _LoginPageState extends State<LoginPage> {
         return errorDialog(context);
       } else {
         // logged in successfully :)
-        AppConstants.guestButtonEnabled=false;
+        AppConstants.guestButtonEnabled = false;
 
         SharedPreferences prefs = await SharedPreferences.getInstance();
         await prefs.setString(
