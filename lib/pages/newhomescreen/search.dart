1e70f9804edaac7cce8c30164c200b2a46c48609
ItsAdityaKSingh <adityakksingh23@gmail.com>
2021-12-08 16:05:39 +0530

    commit on newhomescreen

--- lib/pages/newhomescreen/search.dart
+++ lib/pages/newhomescreen/search.dart
@@ -16,6 +16,27 @@ class Search extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(), body: NoticeBoard(_searchList()));
+    return Scaffold(
+        appBar: AppBar(
+          title: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Search results for "$searchVal"',
+              style: TextStyle(
+                fontFamily: 'Gilroy',
+                color: Colors.white,
+                letterSpacing: .2,
+                fontSize: 21.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          backgroundColor: Color(0xFF176ede),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.only(
+              top: 10.0, left: 5.0, right: 5.0, bottom: 10.0),
+          child: NoticeBoard(_searchList()),
+        ));
   }
 }
